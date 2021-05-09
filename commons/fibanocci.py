851205bea36c465476b58d28e80145cd7dead340
mohan78 <mohanroger63@gmail.com>
2021-05-09 23:17:47 +0530

    Added fibanocci program

--- commons/fibanocci.py
+++ commons/fibanocci.py
@@ -7,8 +7,10 @@
 from functools import lru_cache
 
 
-@lru_cache
+@lru_cache()
 def fibonnaci(num):
+    # We are using lru_cache to store the recent output value that this function has returned.
+    # This is an example of dynamic programming.
     if num == 1:
         return 1
     elif num == 2:
@@ -16,10 +18,11 @@ def fibonnaci(num):
     else:
         return fibonnaci(num-1) + fibonnaci(num-2)
 
+
 def main():
     n_term = int(input("Enter a number: "))
     for i in range(1, n_term+1):
-        print(f"{i} : {fibonnaci(i)}")
+        print(f"{i} term : {fibonnaci(i)}")
 
 
 if __name__ == '__main__':
