c3e2f8d090aafcc94b18bac9bde15d3ec45d85c6
mohan78 <mohanroger63@gmail.com>
2021-05-13 19:06:27 +0530

    Added intro

---
+++ oop/inheritance.py
@@ -0,0 +1,10 @@
+"""
+    Inheritance in Object Oriented terms is the method of acquiring access to attributes and methods from one class by
+    another class. The class from which attributes and methods are acquired is called "parent" class and the one which
+    acquires is called "child" class.
+
+    Syntax:
+    
+    class ChildClass(ParentClass):
+        pass
+"""
